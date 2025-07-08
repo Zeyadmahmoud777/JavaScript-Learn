444e5fd9197539e62e42988b24b07835f483ba77
Zeyadmahmoud777 <zeyadmahmoudali00@gmail.com>
2025-07-08 15:38:17 +0300

    step 3

---
+++ main.js
@@ -0,0 +1,63 @@
+//document.getElementById("count-el").innerText = 5; // Example of setting the count to 5
+
+// let count = 0;
+// console.log(count);
+// let MyAge = 17;
+// console.log(MyAge);
+
+// let firstBatch = 5;
+// let secondBatch = 7;
+// let count = firstBatch + secondBatch;
+// console.log(count);
+
+// let myage = 35;
+// let humanDogRatio = 7;
+
+// let myDogAge = myage * humanDogRatio;
+// console.log(myDogAge);
+
+// let bounsPoints = 50;
+// console.log(bounsPoints);
+
+// bounsPoints = bounsPoints + 50;
+// console.log(bounsPoints);
+
+// bounsPoints = bounsPoints - 75;
+// console.log(bounsPoints);
+
+// bounsPoints = bounsPoints + 45;
+// console.log(bounsPoints);
+
+// function increment() {
+//     // console.log("The button was cliked");
+// }
+
+// function countdown() {
+//     console.log(5);
+//     console.log(4);
+//     console.log(3);
+//     console.log(2);
+//     console.log(1);
+// }
+// countdown();
+
+// function numG() {
+//     console.log(42);
+// }
+// numG();
+
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+// function logLapTime() {
+//     let totalTime = lap1 + lap2 + lap3;
+//     console.log(totalTime);
+// }
+// logLapTime();
+
+let count = 0;
+
+function increment() {
+    count = count + 1;
+    console.log(count);
+}
