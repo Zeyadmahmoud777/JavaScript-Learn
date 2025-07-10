e1587ae593a3a151300f720e563ecbb13da856c2
Zeyadmahmoud777 <zeyadmahmoudali00@gmail.com>
2025-07-10 13:38:11 +0300

    step 3

--- main.js
+++ main.js
@@ -103,5 +103,25 @@ function save() {
     console.log(count);
 }
 save();
+let countEl = document.getElementById("count-el");
+
+console.log(countEl);
+
+let count = 0;
+
+function increment() {
+    count = count + 1;
+    countEl.innerText = count;
+    console.log(count);
+}
+function decrement() {
+    count = count - 1;
+    countEl.innerText = count;
+    console.log(count);
+}
+function save() {
+    console.log(count);
+}
+save();
 
 //*dk 49:07 #20
