f927727aa6a560d6a2cb73e9bd03a7d2352304c1
Zeyadmahmoud777 <zeyadmahmoudali00@gmail.com>
2025-07-12 17:49:07 +0300

    step 4

---
+++ javascript/blackGame.js
@@ -0,0 +1,10 @@
+let openSet = document.getElementById("openset");
+let sttIng = document.getElementById("stting");
+let SttingEl = document.getElementById("stting-el");
+let setList = document.getElementById("setlist");
+
+openSet.addEventListener("click", openSet);
+
+function openSet() {
+    
+};
