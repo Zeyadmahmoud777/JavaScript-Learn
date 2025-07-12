f7773da187ab2e9d51c261cb63bfe52b6501580b
Zeyadmahmoud777 <zeyadmahmoudali00@gmail.com>
2025-07-12 21:43:15 +0300

    step 4

--- javascript/blackGame.js
+++ javascript/blackGame.js
@@ -1,10 +1,17 @@
-let openSet = document.getElementById("openset");
 let sttIng = document.getElementById("stting");
 let SttingEl = document.getElementById("stting-el");
-let setList = document.getElementById("setlist");
-
-openSet.addEventListener("click", openSet);
-
-function openSet() {
-    
+let closeEl = document.getElementById("close-el");
+let contGame = document.getElementById("continue-el");
+sttIng.addEventListener("click", sttIng);
+closeEl.addEventListener("click", close);
+function openset() {
+    sttIng = SttingEl.style.display = "block";
 };
+function close() {
+   sttIng = SttingEl.style.display = "none";
+};
+function Continue() {
+    sttIng = SttingEl.style.display = "none";
+    contGame = document.getElementById("continue-el").style.display = "block";
+};
+
