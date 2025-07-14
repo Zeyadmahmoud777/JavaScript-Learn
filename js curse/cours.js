fcd5e35793468b5bc31d0f30e72e25b767155d47
Zeyadmahmoud777 <zeyadmahmoudali00@gmail.com>
2025-07-14 16:41:09 +0300

    step 4

---
+++ js curse/cours.js
@@ -0,0 +1,18 @@
+let firstCard = 11;
+
+let secondCard = 10;
+
+let sum = firstCard + secondCard;
+
+//
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+    console.log("Blackjack!😎");
+} else if (sum > 21) {
+    console.log("You Lost 😶?");
+};
+
+
+//* 1:39:48
