f8f767f1e17455d66c8e5e0da091400e43a1c282
Zeyadmahmoud777 <zeyadmahmoudali00@gmail.com>
2025-07-17 16:00:56 +0300

    step 4

--- js curse/cours.js
+++ js curse/cours.js
@@ -1,34 +1,39 @@
 let firstCard = 11;
-
 let secondCard = 10;
-
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-//
 
-if (sum < 21) {
+
+let messageEl = document.getElementById("message-el");
+//let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
+
+let cardsEl = document.getElementById("cards-el");
+
+function startGame() {
+    cardsEl.textContent = "Cards: " +  firstCard + " " + secondCard;
+    sumEl.textContent = "sum: " + sum;
+    if (sum < 21) {
     message = "Do you want to draw a new card?";
 } else if (sum === 21) {
-    message = "Blackjack!😎";
+    message = "Blackjack!";
     hasBlackJack = true;
 } else {
-    message = "You Lost 😶?";
+    message = "You Lost?";
     isAlive = false;
 };
+messageEl.textContent = message
+};
 
-console.log(message);
-console.log(hasBlackJack);
-//* 1:39:48
 
+
+
+//* 2:21:46
 // let age = 21;
-
 // if (age <= 20) {
 //     console.log("Ypu can not entet the club");
 // } else {
 //     console.log("welcome");
 // };
-
-//* 2:02:46
-
