4facb3e46c842f0cd2a3b6604ccfd59e703c9a49
Zeyadmahmoud777 <zeyadmahmoudali00@gmail.com>
2025-07-21 17:05:02 +0300

    step 4

--- js curse/cours.js
+++ js curse/cours.js
@@ -1,25 +1,40 @@
 //&GAME start
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.querySelector("#sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+console.log(cards);
+
+function getRandomCard() {
+     let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber > 10) {
+        return 10;
+    } else if(randomNumber === 1){
+        return 11;
+    } else {
+       return randomNumber;
+    };
+};
+// return getRandomCard;
+
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     renderGame();
 };
 function renderGame() {
     cardsEl.textContent = "Cards: ";
-
     for (let i = 0; i < cards.length; i++){
         cardsEl.textContent += cards[i] + " ";
     };
-   
     sumEl.textContent = "sum: " + sum;
     if (sum < 21) {
     message = "Do you want to draw a new card?";
@@ -28,9 +43,9 @@ function renderGame() {
     hasBlackJack = true;
 } else {
     message = "You Lost?";
-    isAlive = false;
-};
-messageEl.textContent = message
+    isAlive = true;
+}
+    messageEl.textContent = message;
 };
 
 function newCard() {
@@ -38,10 +53,26 @@ function newCard() {
     sum += card;
     cards.push(card);
     console.log(cards);
-    renderGame()
+    renderGame();
 };
 //&GAME END
 //*3:17:09 return lesson;
+// let player1Time = 102;
+// let player2Time = 107;
+
+// function getFastestRaceTime() {
+//     if (player1Time < player2Time) {
+//         return player1Time;
+//     } else if (player2Time < player1Time) {
+//         return player2Time;
+//     } else {
+//         return player1Time;
+//     };
+// };
+// let fastestRace = getFastestRaceTime();
+// console.log(fastestRace);
+
+
 
 // let per = ["per boragan", 35, true];>>>>>>//*Arreys is here;
 // console.log(per[1]);
